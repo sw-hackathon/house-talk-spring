2779dd8ff50fbe9b4ee2527ee1f189608cecfb55
^__^ BaekGyu <hana222966@naver.com>
2022-09-14 22:31:54 +0900

    refactor : change response date time List -> Optional List in find Post by home Id repository method

--- src/main/java/com/house/talk/domain/community/dao/PostRepository.java
+++ src/main/java/com/house/talk/domain/community/dao/PostRepository.java
@@ -5,7 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post> findByHome_id(@Param("homeId") Long homeId);
+    Optional<List<Post>> findByHome_id(@Param("homeId") Long homeId);
 }
